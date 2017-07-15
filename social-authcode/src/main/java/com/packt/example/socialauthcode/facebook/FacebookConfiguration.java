bd14fc51e9ca64e0e9d814cedf805cbb843fb78a
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-07-15 15:02:35 -0300

    Removes unecessary annotation

--- social-authcode/src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
+++ social-authcode/src/main/java/com/packt/example/socialauthcode/facebook/FacebookConfiguration.java
@@ -2,9 +2,7 @@ package com.packt.example.socialauthcode.facebook;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.social.FacebookProperties;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -19,7 +17,6 @@ import org.springframework.social.facebook.api.Facebook;
 
 @Configuration
 @EnableSocial
-@EnableConfigurationProperties(FacebookProperties.class)
 public class FacebookConfiguration extends SocialAutoConfigurerAdapter {
 
 	@Autowired
