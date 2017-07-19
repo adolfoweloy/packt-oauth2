e5d2d5e85d139053cd37990af84b6b9ef1f4d990
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-07-19 08:23:00 -0300

    add error page and define the class OAuth2AuthorizationServer as configuration

--- chapter-2/rdbm-server/src/main/java/com/packt/example/rdbmserver/OAuth2AuthorizationServer.java
+++ chapter-2/rdbm-server/src/main/java/com/packt/example/rdbmserver/OAuth2AuthorizationServer.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+@Configuration
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServer extends
 		AuthorizationServerConfigurerAdapter {
