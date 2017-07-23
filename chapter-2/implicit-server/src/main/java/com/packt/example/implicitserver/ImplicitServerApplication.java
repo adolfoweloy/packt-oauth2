7445850530be769a888c55b3bded8acf20a93af7
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-07-23 20:17:00 -0300

    [chapter-2] remove unecessary dependencies and configurations

--- chapter-2/implicit-server/src/main/java/com/packt/example/implicitserver/ImplicitServerApplication.java
+++ chapter-2/implicit-server/src/main/java/com/packt/example/implicitserver/ImplicitServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImplicitServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImplicitServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImplicitServerApplication.class, args);
+    }
 }
