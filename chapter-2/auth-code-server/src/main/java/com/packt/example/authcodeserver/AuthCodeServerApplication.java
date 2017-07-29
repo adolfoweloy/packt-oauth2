5a0d2c3bd0b779a792c72933f61e462ef6eb6edd
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-07-28 22:54:37 -0300

    [chapter 2] update version of spring security oauth2 for authorization code recipe

--- chapter-2/auth-code-server/src/main/java/com/packt/example/authcodeserver/AuthCodeServerApplication.java
+++ chapter-2/auth-code-server/src/main/java/com/packt/example/authcodeserver/AuthCodeServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthCodeServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthCodeServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCodeServerApplication.class, args);
+    }
 }
