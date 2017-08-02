719264a2c12dbed4a1dd00e555c3c8b84d72eaf5
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-08-02 20:44:02 -0300

    [chapter 3] create recipe for resource owner password credentials client

---
+++ chapter-3/client-password/src/test/java/com/packt/example/clientpassword/ClientPasswordApplicationTests.java
@@ -0,0 +1,16 @@
+package com.packt.example.clientpassword;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientPasswordApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
