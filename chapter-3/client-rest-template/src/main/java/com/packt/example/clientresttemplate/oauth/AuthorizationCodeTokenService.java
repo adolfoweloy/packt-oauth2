05e0d7b38a1676869faf82635eda304e7941c724
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-08-06 21:42:49 -0300

    [chapter 3] remove oauth dependency, and break the responsibilities for rest template recipe

--- chapter-3/client-rest-template/src/main/java/com/packt/example/clientresttemplate/oauth/AuthorizationCodeTokenService.java
+++ chapter-3/client-rest-template/src/main/java/com/packt/example/clientresttemplate/oauth/AuthorizationCodeTokenService.java
@@ -4,27 +4,25 @@ import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AuthorizationCodeTokenService {
+    //@formatter:off
+    @Autowired
+    private AuthorizationCodeConfiguration configuration;
 
     public String getAuthorizationEndpoint() {
-
         String endpoint = "http://localhost:8080/oauth/authorize";
 
         Map<String, String> authParameters = new HashMap<>();
@@ -38,7 +36,6 @@ public class AuthorizationCodeTokenService {
     }
 
     private String buildUrl(String endpoint, Map<String, String> parameters) {
-        //@formatter:off
         List<String> paramList = new ArrayList<>(parameters.size());
 
         parameters.forEach((name, value) -> {
@@ -47,7 +44,6 @@ public class AuthorizationCodeTokenService {
 
         return endpoint + "?" + paramList.stream()
               .reduce((a, b) -> a + "&" + b).get();
-        //@formatter:on
     }
 
     private String getEncodedUrl(String url) {
@@ -60,12 +56,13 @@ public class AuthorizationCodeTokenService {
 
     public OAuth2Token getToken(String authorizationCode) {
         RestTemplate rest = new RestTemplate();
-        String authBase64 = encodeCredentials("clientapp", "123456");
+        String authBase64 = configuration.encodeCredentials("clientapp",
+                "123456");
 
         RequestEntity<MultiValueMap<String, String>> requestEntity = new RequestEntity<>(
-                getBody(authorizationCode), getHeader(authBase64),
-                HttpMethod.POST,
-                URI.create("http://localhost:8080/oauth/token"));
+            configuration.getBody(authorizationCode),
+            configuration.getHeader(authBase64), HttpMethod.POST,
+            URI.create("http://localhost:8080/oauth/token"));
 
         ResponseEntity<OAuth2Token> responseEntity = rest.exchange(
                 requestEntity, OAuth2Token.class);
@@ -77,31 +74,5 @@ public class AuthorizationCodeTokenService {
         throw new RuntimeException("error trying to retrieve access token");
     }
 
-    public String encodeCredentials(String username, String password) {
-        String credentials = username + ":" + password;
-        String encoded = new String(Base64.getEncoder().encode(
-                credentials.getBytes()));
-
-        return encoded;
-    }
-
-    private MultiValueMap<String, String> getBody(String authorizationCode) {
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("grant_type", "authorization_code");
-        formData.add("scope", "read_profile");
-        formData.add("code", authorizationCode);
-        formData.add("redirect_uri", "http://localhost:9000/callback");
-        return formData;
-    }
-
-    private HttpHeaders getHeader(String clientAuthentication) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-
-        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        httpHeaders.add("Authorization", "Basic " + clientAuthentication);
-
-        return httpHeaders;
-    }
-
+    // @formatter:on
 }
