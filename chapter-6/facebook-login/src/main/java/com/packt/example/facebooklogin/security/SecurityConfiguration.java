61cdff8acf396dce3bc899e3b589b953cd323b75
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-09-16 20:31:26 -0300

    [chapter 6] create first version of the application that will use facebook login

---
+++ chapter-6/facebook-login/src/main/java/com/packt/example/facebooklogin/security/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.packt.example.facebooklogin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/").permitAll().and()
+            .authorizeRequests()
+            .antMatchers("/profile/*").authenticated().and()
+            .authorizeRequests().anyRequest().authenticated().and()
+            .httpBasic().and()
+            .csrf().disable();
+    }
+
+}
