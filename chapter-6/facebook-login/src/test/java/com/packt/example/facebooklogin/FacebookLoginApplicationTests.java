98268f8510a3633dbc326f323544f48df5f8f4e8
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-09-16 14:16:08 -0300

    [chapter 6] creating recipe for facebook login support

---
+++ chapter-6/facebook-login/src/test/java/com/packt/example/facebooklogin/FacebookLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.packt.example.facebooklogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacebookLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
