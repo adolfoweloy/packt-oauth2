3d3f8b1c1522b8238a0114b50078f4aba902c8ae
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-10-02 02:10:27 -0300

    [chapter 7] add support for implicit and password grant types

--- chapter-7/server/src/main/java/example/packt/com/server/OAuth2Provider.java
+++ chapter-7/server/src/main/java/example/packt/com/server/OAuth2Provider.java
@@ -1,12 +1,15 @@
 package example.packt.com.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 public class OAuth2Provider {
@@ -14,6 +17,14 @@ public class OAuth2Provider {
     @EnableAuthorizationServer
     public static class AuthorizationServer
         extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -24,7 +35,10 @@ public class OAuth2Provider {
                 .redirectUris(
                         "oauth2://profile/callback",
                         "http://localhost:9000/callback")
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes(
+                        "authorization_code",
+                        "implicit",
+                        "password")
                 .scopes("read_profile", "read_contacts");
             //@formatter:on
         }
